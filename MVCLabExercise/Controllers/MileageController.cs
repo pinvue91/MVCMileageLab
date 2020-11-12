e9a9dbe53abd83291ea283bfa0f3b1a803de05e9
pinvue91 <pin.vue@hotmail.com>
2020-11-11 23:05:13 -0500

    MVC Mileage Exercise

---
+++ MVCLabExercise/Controllers/MileageController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MVCLabExercise.Controllers
+{
+    public class MileageController : Controller
+    {
+        public IActionResult MileageForm()
+        {
+            return View();
+        }
+
+        public IActionResult MileageResult(double mpg, double gallons)
+        {
+            double range = mpg * gallons;
+
+            return View("MileageResult", range);
+        }
+
+
+    }
+}
